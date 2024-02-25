367b6892eb81545f917835fcdf7119ee589f91b3
Darshan Panchal <panchaldarshan121@gmail.com>
2024-02-25 12:48:03 +0530

    find sum of n natural numbers

---
+++ src/main/java/miscproblems/FindSum.java
@@ -0,0 +1,12 @@
+package miscproblems;
+// find sum of n natural numbers
+
+public class FindSum {
+    static int findSum(int n){
+        return n * (n+1)/2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(FindSum.findSum(99999));
+    }
+}
