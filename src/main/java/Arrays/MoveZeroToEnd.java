d9cb68c44d1fed92e02a51431f1377e06acdd954
Darshan Panchal <panchaldarshan121@gmail.com>
2024-03-02 12:04:56 +0530

    move zeroes to the end of the array

---
+++ src/main/java/Arrays/MoveZeroToEnd.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+import java.util.Arrays;
+
+// move zeroes to the end of an array.
+public class MoveZeroToEnd {
+    static String moveZeroToEnd(int[] arr) {
+        int j = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0 && arr[j] == 0) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+            if (arr[j] != 0) {
+                j++;
+            }
+
+        }
+        return Arrays.toString(arr);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 0, 2, 0, 3, 0};
+        System.out.println(moveZeroToEnd(arr));
+
+    }
+}
