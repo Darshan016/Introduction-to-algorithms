cc866087eb3a767931a22273a0542dc2abb7350d
Darshan Panchal <panchaldarshan121@gmail.com>
2024-03-10 14:45:19 +0530

    ListNode class for doubly linked list

---
+++ src/main/java/org/example/DoublyLinkedList/ListNode.java
@@ -0,0 +1,11 @@
+package org.example.DoublyLinkedList;
+
+public class ListNode {
+    protected int data;
+    protected ListNode previous;
+    protected ListNode next;
+
+    public ListNode(int data) {
+        this.data = data;
+    }
+}
