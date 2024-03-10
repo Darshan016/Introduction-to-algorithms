f64ca35009969a1aab544918806dd6e125dea96f
Darshan Panchal <panchaldarshan121@gmail.com>
2024-03-10 15:54:54 +0530

    methods to add at the beginning of the DLL

---
+++ src/main/java/org/example/DoublyLinkedList/DoublyLinkedList.java
@@ -0,0 +1,57 @@
+package org.example.DoublyLinkedList;
+
+import java.util.List;
+
+public class DoublyLinkedList {
+    private ListNode head;
+    private ListNode tail;
+    private int length;
+
+    public DoublyLinkedList() {
+        head = null;
+        tail = null;
+        length = 0;
+    }
+
+    public boolean isEmpty() {
+        return length == 0;
+    }
+
+    public int length() {
+        return length;
+    }
+
+    //traverse linked list in forward direction
+    public void printForward() {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "-->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    // traverse linked list in backward direction
+    public void printBackward() {
+        ListNode temp = tail;
+        while (temp != null) {
+            System.out.print(temp.data + "-->");
+            temp = temp.previous;
+        }
+        System.out.println("null");
+    }
+
+    //add at the beginning of the list
+    public void addFirst(int value) {
+        ListNode listNode = new ListNode(value);
+        if (isEmpty()) {
+            tail = listNode;
+        } else {
+            head.previous = listNode;
+        }
+        listNode.next = head;
+        head = listNode;
+        length++;
+
+    }
+}
