e4fa25183cce53e5c80a93bb6c5c3840f8a5fe7a
Darshan Panchal <panchaldarshan121@gmail.com>
2024-03-31 15:04:55 +0530

    methods to print values of a queue and dequeue algo

--- src/main/java/org/example/Queue/Queue.java
+++ src/main/java/org/example/Queue/Queue.java
@@ -1,9 +1,15 @@
 package org.example.Queue;
 
 public class Queue {
-    private ListNode front;
-    private ListNode rear;
-    private int length;
+    protected ListNode front;
+    protected ListNode rear;
+    protected int length;
+
+    public Queue() {
+        this.front = null;
+        this.rear = null;
+        this.length = 0;
+    }
 
     public boolean isEmpty() {
         return length == 0;
@@ -13,14 +19,39 @@ public class Queue {
         return length;
     }
 
-    public void enqueue(int data){
+    public void enqueue(int data) {
         ListNode listNode = new ListNode(data);
-        if (isEmpty()){
+        if (isEmpty()) {
             front = listNode;
-        }else{
+        } else {
             rear.next = listNode;
         }
         rear = listNode;
         length++;
+    }
+
+    public void print() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty.");
+        }
+        ListNode current = front;
+        while (current != null) {
+            System.out.print(current.data + "-->");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+    public int dequeue() {
+        if (isEmpty()) {
+            System.out.println("Queue is already empty.");
+        }
+        int result = front.data;
+        front = front.next;
+        if (front == null) {
+            rear = null;
+        }
+        length--;
+        return result;
     }
 }
