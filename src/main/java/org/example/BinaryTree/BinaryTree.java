04952c0a2ab34ea4deb508ea6a5f283b9cffa66f
Darshan Panchal <panchaldarshan121@gmail.com>
2024-04-06 16:12:11 +0530

    iterative inorder traversal

--- src/main/java/org/example/BinaryTree/BinaryTree.java
+++ src/main/java/org/example/BinaryTree/BinaryTree.java
@@ -48,12 +48,30 @@ public class BinaryTree {
         }
     }
 
-    public void inOrderTraversal(TreeNode root){
-        if (root == null){
+    public void inOrderTraversal(TreeNode root) {
+        if (root == null) {
             return;
         }
         inOrderTraversal(root.left);
-        System.out.print(root.data+" ");
+        System.out.print(root.data + " ");
         inOrderTraversal(root.right);
+    }
+
+    public void inOrderTraversalIterative(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode temp = root;
+        while (!stack.isEmpty() || temp != null) {
+            if (temp != null) {
+                stack.push(temp);
+                temp = temp.left;
+            } else {
+                temp = stack.pop();
+                System.out.print(temp.data + " ");
+                temp = temp.right;
+            }
+        }
     }
 }
