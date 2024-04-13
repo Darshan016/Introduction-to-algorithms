554020f2c992959607c691621700257f413a7bea
Darshan Panchal <panchaldarshan121@gmail.com>
2024-04-13 13:26:00 +0530

    postorder traversal iterative approach

--- src/main/java/org/example/BinaryTree/BinaryTree.java
+++ src/main/java/org/example/BinaryTree/BinaryTree.java
@@ -81,4 +81,27 @@ public class BinaryTree {
         postOrderTraversal(root.right);
         System.out.print(root.data + " ");
     }
+
+    public void postOrderIterative(TreeNode root) {
+        TreeNode current = root;
+        Stack<TreeNode> stack = new Stack<>();
+        while (current != null || !stack.isEmpty()) {
+            if (current != null) {
+                stack.push(current);
+                current = current.left;
+            } else {
+                TreeNode temp = stack.peek().right;
+                if (temp == null) {
+                    temp = stack.pop();
+                    System.out.print(temp.data + " ");
+                    while (!stack.isEmpty() && temp == stack.peek().right) {
+                        temp = stack.pop();
+                        System.out.print(temp.data + " ");
+                    }
+                } else {
+                    current = temp;
+                }
+            }
+        }
+    }
 }
