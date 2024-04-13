38953387f46ec9bcfa3a13a7368a075782c5e295
Darshan Panchal <panchaldarshan121@gmail.com>
2024-04-13 15:21:28 +0530

    find max element

--- src/main/java/org/example/BinaryTree/BinaryTree.java
+++ src/main/java/org/example/BinaryTree/BinaryTree.java
@@ -1,5 +1,7 @@
 package org.example.BinaryTree;
 
+import com.sun.source.tree.Tree;
+
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
@@ -123,5 +125,21 @@ public class BinaryTree {
                 queue.offer(temp.right);
             }
         }
+    }
+
+    public int findMax(TreeNode root) {
+        if (root == null) {
+            return Integer.MIN_VALUE;
+        }
+        int result = root.data;
+        int left = findMax(root.left);
+        int right = findMax(root.right);
+        if (left > result) {
+            result = left;
+        }
+        if (right > result) {
+            result = right;
+        }
+        return result;
     }
 }
