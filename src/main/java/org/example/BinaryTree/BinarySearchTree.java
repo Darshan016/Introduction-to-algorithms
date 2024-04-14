3f431d389211ce0f00e452b9eba5c8dbbc75e4b5
Darshan Panchal <panchaldarshan121@gmail.com>
2024-04-14 14:43:19 +0530

    insert method implementation

---
+++ src/main/java/org/example/BinaryTree/BinarySearchTree.java
@@ -0,0 +1,31 @@
+package org.example.BinaryTree;
+
+public class BinarySearchTree {
+    protected TreeNode root;
+
+    public TreeNode insert(TreeNode root, int value) {
+        if (root == null) {
+            root = new TreeNode(value);
+            return root;
+        }
+        if (value < root.data) {
+            root.left = insert(root.left, value);
+        } else {
+            root.right = insert(root.right, value);
+        }
+        return root;
+    }
+
+    public void insert(int value) {
+        root = insert(root, value);
+    }
+
+    public void inOrder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.data + " ");
+        inOrder(root.right);
+    }
+}
